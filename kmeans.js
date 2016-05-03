aa21201765fc6d24eb9237aeb4ee714753325661
Innocenzo Biondo <innocenzobiondo@gmail.com>
2016-05-03 22:42:54 +0200

    Update

--- kmeans.js
+++ kmeans.js
@@ -22,50 +22,41 @@ var userTimelineSchema = new Schema({
           vetMacro: {}
 });
 
-var clusterSchema = new Schema({
-        IdCluster : [],
-        MacroCluster : []
-});
-
 userTimelineSchema.plugin(uniqueValidator);
 var usertimeline = mongoose.model('usertimelines', userTimelineSchema);
 
-clusterSchema.plugin(uniqueValidator);
-var cluster = mongoose.model('clusters', clusterSchema);
 
 //functions
-
-function	saveVenues(venues){
-	venues.save(function (err) {
-		if (err) {
-
-			count2++;
-			console.log("cluster duplicate = "+ count2);
-		}
-    else{
-			count3++;
-			console.log("new cluster saved --------------->   "+venues);
-			console.log("Number cluster save = "+count3);
-		}
-	});
-  return;
-};
 
 // compare vectors
 function arraycompare(a1,b1){
   return JSON.stringify(a1)==JSON.stringify(b1);
 }
+//cosine-similarity
+function dotproduct(a,b) {
+    var n = 0, lim = Math.min(a.length,b.length);
+    for (var i = 0; i < lim; i++) n += a[i] * b[i];
+    return n;
+ }
+
+function norm2(a) {var sumsqr = 0; for (var i = 0; i < a.length; i++) sumsqr += a[i]*a[i]; return Math.sqrt(sumsqr);}
+
+function similarity(a, b) {return dotproduct(a,b)/(norm2(a)*norm2(b));}
 
 //kmeans
-function calculate_kmeans(VectorMacro,VectorId) {
+function calculate_kmeans(data) {
   var SimilarUser=[];
-
+  var VectorMacro =[];
+  var VectorId=[];
   var IdCluster=[];
   var MacroCluster=[];
   var ClusterListMacro=[];
   var count=0;
 
-
+  for (var i = 0 ; i < data.length ; i++){
+    VectorMacro.push( data[i]['vetMacro'] );
+    VectorId.push( data[i]['id'] );
+  }
   //k= number of elements of the weighted vector
   kmeans.clusterize(VectorMacro, {k: 128}, function(err,res) {
     if (err) {
@@ -76,40 +67,129 @@ function calculate_kmeans(VectorMacro,VectorId) {
 
       //for every cluster push in IdCluster the cluster of id  that belong to that cluster
       //for every cluster push in MacroCluster the cluster of vectorMacro that belong to that cluster
-      async.each(res, function(res) {
-        console.log(res.cluster);
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].cluster);
         var vectid=[];
         var vectmacro=[];
-        for (var j = 0; j < res.clusterInd.length; j++) {
+        for (var j = 0; j < res[i].clusterInd.length; j++) {
 
-            vectid.push(  VectorId[res.clusterInd[j]]);
-            vectmacro.push(  VectorMacro[res.clusterInd[j]]);
+            vectid.push(  VectorId[res[i].clusterInd[j]]);
+            vectmacro.push(  VectorMacro[res[i].clusterInd[j]]);
         }
         IdCluster.push(vectid) ;
         MacroCluster.push(vectmacro);
-      })
-      var newdata= {
-                      'IdCluster' : IdCluster,
-                      'MacroCluster' : MacroCluster
-                    }
-                    console.log(newdata);
-      saveVenues(new cluster(newdata));
-//      console.log(IdCluster);
-  //    console.log(MacroCluster);
+      }
+
     }
 
-  })
+    var SimilarPlaces=[];
+    var SimilarPlaces2=[];
+    var VisitedPlaces=[];
+    var Position=0;
+    var NumberCluster=0;
+    var ClusterListId=[];
+    var Similitude=[];
+    var vettore=[];
+    var a=[];
 
+    //find the number of the cluster in witch "company" is
+    for (var i = 0; i < IdCluster.length; i++) {
+      for (var j = 0; j < IdCluster[i].length; j++) {
+        if (IdCluster[i][j]==company){
+          NumberCluster=i;
+          Position=j;
+          break;
+        }
+      }
+    }
+
+    for (var i = 0; i < IdCluster[NumberCluster].length; i++) {
+      ClusterListId.push(IdCluster[NumberCluster][i]);
+    }
+    console.log(ClusterListId);
+    //push in Similitude all the users that belong to the same cluster of "company" and their similarity with company
+    for (var i = 0; i < MacroCluster[NumberCluster].length; i++) {
+      if (i!=Position) {
+        Similitude.push({"user":ClusterListId[i],"similarity": similarity(MacroCluster[NumberCluster][Position],MacroCluster[NumberCluster][i])} )
+      }
+    }
+
+
+    Similitude.sort(function(a, b) {
+      return parseFloat(b.similarity) - parseFloat(a.similarity);
+    })
+console.log(Similitude);
+for (var i = 0; i < 5; i++) {
+  a.push(Similitude[i]);
 }
-var count3=0;
-var count2=0;
-var VectorMacro =[];
-var VectorId=[];
+
+    //find the three users more similar and return two json: one with the places that "company" has visited
+
+    //the other with the places in witch his more similar users have visited
+    async.each(a, function(pro) {
+      console.log(pro.user);
+
+      var stream = usertimeline.find({"id":pro.user}).stream()
+      stream.on('data',function(doc2) {
+        console.log(pro);
+
+        for (var i = 0; i < doc2.venues.length; i++) {
+        SimilarPlaces2.push(doc2.venues[i]);
+        }
+        var newdata={
+          'venues':SimilarPlaces2,
+          'user':doc2.id,
+          'similarity':pro.similarity
+
+        }
+        vettore.push(newdata);
+        SimilarPlaces2=[];
+      }).on('error', function (err) {
+
+        console.log(err);
+
+      }).on('close', function () {
+
+       var file = '/home/enzo/Documenti/SII/predizione -punti-di-interesse.git/DaVisitare.json';
+       var obj = {id_instagram:vettore};
+       jsonfile.writeFile(file, obj, function (err) {
+
+         console.error(err);
+
+      })
+      });
+    })
+
+    var stream = usertimeline.find({"id":company}).stream()
+    stream.on('data',function(doc2) {
+      for (var i = 0; i < doc2.venues.length; i++) {
+        VisitedPlaces.push(doc2.venues[i]);
+      }
+    }).on('error', function (err) {
+
+      console.log(err);
+
+    }).on('close', function () {
+
+     var file = '/home/enzo/Documenti/SII/predizione -punti-di-interesse.git/Visitati.json';
+     var obj = {id_instagram:VisitedPlaces,
+                  user: company};
+     jsonfile.writeFile(file, obj, function (err) {
+
+       console.error(err);
+
+    })
+    });
+
+  })
+}
+
+
+//user to match
+var company='239254149';
 //global variables
 var WeightedList = [];
 var VectorZero=[0,0,0,0,0,0,0,0];
-var count2=0
-var count3=0;
 //definition and start stream
 var stream = usertimeline.find().stream();
 stream.on('data',function(doc2) {
@@ -121,22 +201,19 @@ for (var variable in doc2.vetMacro) {
     WeightMacro.push(doc2.vetMacro[variable]);
   }
 }
-
 //filter out all the elements of the db that have as vetMacro propriety a vector of 0
 if (!arraycompare(WeightMacro,VectorZero)&&WeightMacro.length==8) {
-  //WeightedList.push({"id": doc2.id, "vetMacro":WeightMacro });
-  VectorMacro.push( WeightMacro );
-  VectorId.push( doc2.id );
+  WeightedList.push({"id": doc2.id, "vetMacro":WeightMacro });
 }
-//console.log(WeightedList.length);
+
 }).on('error', function (err) {
 
 console.log(err);
 
 }).on('close', function () {
 
-console.log();
+console.log(WeightedList);
 //call the function to calculate kmeans
-calculate_kmeans(VectorMacro,VectorId);
+calculate_kmeans(WeightedList);
 
 });
